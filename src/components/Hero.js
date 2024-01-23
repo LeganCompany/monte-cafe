61a61ba8ab64cec77574592b9b3c617e0ce1a1da
ider514 <ider0514@gmail.com>
2024-01-23 18:05:46 +1100

    initial commit, added background image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,23 @@
+import React from 'react';
+import background from './../img/frame1.png';
+
 // Component for displaying the very first screen of the website. (Welcome to Monte Cafe)
 function Hero() {
-  return (
-    <div>
-      <h1>Hero</h1>
-    </div>
-  );
+	const divStyle = {
+		backgroundImage: `url(${background})`,
+		backgroundSize: 'cover',
+		width: '100%',
+		height: '914px',
+		// display: 'flex',
+		// justifyContent: 'center',
+		// alignItems: 'center',
+	};
+
+	return (
+		<div style={divStyle}>
+			<h1>Hello</h1>
+		</div>
+	);
 }
 
 export default Hero;
